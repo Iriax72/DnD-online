778fad25bb1915883835139f95d90c329337220d
flexult <touma.leandre.72@gmail.com>
2025-09-22 18:09:47 +0000

    patch css et js

--- php/head.php
+++ php/head.php
@@ -3,6 +3,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Site pour jouer à DnD en ligne">
     <title>DnD-Online</title>
-    <link rel="stylesheet" href="/style.css">
-    <script src="/script.js" defer></script>
+    <link rel="stylesheet" href="/DnD-online/style.css">
+    <script src="/DnD-online/script.js" defer></script>
 </head>
