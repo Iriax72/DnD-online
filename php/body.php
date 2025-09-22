03a61902f1827ceda806c7c7cdc9257a78728d46
flexult <touma.leandre.72@gmail.com>
2025-09-22 19:51:01 +0000

    modification des buttons en a. plus un peu de clear

--- php/body.php
+++ php/body.php
@@ -1,6 +1,5 @@
 <body class="body_without_nav">
     <header class="header_without_nav">
-        <?php echo getcwd(); ?>
         <?php require './html/header.html'; ?>
     </header>
     <main class="clickToQuitNav">
