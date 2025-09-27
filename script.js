b0411c02da701e20c6f09525865d984b3bb6d86f
flexult <touma.leandre.72@gmail.com>
2025-09-27 06:50:24 +0000

    fermeture nav par clique

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const nav = document.querySelector("nav");
+const nav_buttons = [...nav.children[0].children];
 const nav_button = document.querySelector("#nav_button");
-let isHamburgerOpend = false
+let isNavOpend = false
 const elementsQuitNav = [...document.querySelectorAll(".clickToQuitNav")]
 
 function toggle_nav(){
-    isHamburgerOpend ? nav.classList.add("hidden") : nav.classList.remove("hidden");
-    isHamburgerOpend = !isHamburgerOpend;
+    isNavOpend ? nav.classList.add("hidden") : nav.classList.remove("hidden");
+    isNavOpend = !isNavOpend;
 }
 
 toggle_nav();
@@ -14,8 +15,14 @@ nav_button.onclick = () => toggle_nav();
 
 elementsQuitNav.forEach(e => {
     e.onclick = () => {
-        if(isHamburgerOpend){
+        if(isNavOpend){
             toggle_nav();
         }
+    }
+});
+
+nav_buttons.forEach(b => {
+    b.onclick = () => {
+        toggle_nav();
     }
 });
