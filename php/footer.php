5622c7790318b38a6585745efbd04cfd418503ec
flexult <touma.leandre.72@gmail.com>
2025-09-27 07:08:37 +0000

    petite modif de la <a> du footer

--- php/footer.php
+++ php/footer.php
@@ -5,5 +5,5 @@
     <br/>
     <p>Voici l'heure: (encore flemme de mettre)</p>
     <p>Merci à Chanel-is-Industries et Flaticon pour les icônes gratuites et libres de droits!</p>
-    <a href="https://www.flaticon.com/fr/icones-gratuites/oeuf-de-dragon" title="oeuf de dragon icônes">Oeuf de dragon icônes créées par Chanut-is-Industries</a>
+    <a href="https://www.flaticon.com/fr/icones-gratuites/oeuf-de-dragon" title="oeuf de dragon icônes" target="_blank" rel="noopener noreferrer">Oeuf de dragon icônes créées par Chanut-is-Industries</a>
 </footer>
