ce43c51f8a549fbf0908e96b6e6a111186f7338a
flexult <touma.leandre.72@gmail.com>
2025-09-27 08:14:49 +0000

    rien

--- php/footer.php
+++ php/footer.php
@@ -4,6 +4,6 @@
     <p class="t-white">Nous contacter: (je mettrai le lien plus tard, rappelez moi si je l'ai oublié svp)</p>
     <br/>
     <p class="t-white">Voici l'heure: (encore flemme de mettre)</p>
-    <p class="t-white">Merci à Chanel-is-Industries et Flaticon pour les icônes gratuites et libres de droits!</p>
+    <p class="t-white">Merci à Chanel-is-Industries et Flaticon pour les icônes gratuites et libres de droits! :</p>
     <a href="https://www.flaticon.com/fr/icones-gratuites/oeuf-de-dragon" title="oeuf de dragon icônes" target="_blank" rel="noopener noreferrer">Oeuf de dragon icônes créées par Chanut-is-Industries</a>
 </footer>
