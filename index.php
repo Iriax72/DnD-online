6e9789ccb82b806ce9416145848db264000d7180
flexult <touma.leandre.72@gmail.com>
2025-09-30 20:06:06 +0000

    regrouoement du code dams index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
 $page = $_GET['page'] ?? 'accueil';
-require "./php/sections/$page.php"
 ?>
+
+<DOCTYPE html>
+<html lang="fr">
+    <?php require __DIR__ . '/php/head.php'; ?>
+    <body class="m-0 p-0">
+        <?php
+        require __DIR__ . '/php/header.php';
+        require __DIR__ . '/php/nav.php';
+        ?>
+        <main class="clickToQuitNav">
+            <?php require __DIR__ . "/php/sections/$page.php"; ?>
+        </main>
+        <?php
+        require __DIR__ . '/php/footer.php';
+        ?>
+    </body>
+</html>
--- php/sections/settings.php
+++ php/sections/settings.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<?php
-require __DIR__ . '/../head.php';
-?>
-
-<body class="m-0 p-0">
-    <?php
-    require __DIR__ . '/../header.php';
-    require __DIR__ . '/../nav.php';
-    ?>
-    
-    <main class="clickToQuitNav">
-        <section id="settings">
-            <h2>Réglages</h2>
-        </section>
-    </main>
-    
-    <?php
-    require __DIR__ . '/../footer.php';
-    ?>
-</body>
-</html>
+<section id="settings">
+    <h2>Réglages</h2>
+</section>
