553150220feca1af1ff058630762df8348560ceb
flexult <touma.leandre.72@gmail.com>
2025-10-04 07:40:32 +0000

    patch fomulaire

--- php/form/login.php
+++ php/form/login.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/form.php';
-form('login', 'connexion');
-text_input('pseudo', 'pseudo');
-text_input('password', 'mot de passe');
-close_form('Se connecter');
+echo form('login', 'connexion');
+echo text_input('pseudo', 'pseudo');
+echo text_input('password', 'mot de passe');
+echo close_form('Se connecter');
--- php/form/signin.php
+++ php/form/signin.php
@@ -1,14 +1,14 @@
 <?php
 require_once __DIR__ . '/form.php';
-form('signin', 'Créer un compte');
+echo form('signin', 'Créer un compte');
 ?>
 <p>Nécessaire</p>
 <?php
-text_input('new_pseudo', 'Pseudo');
-text_input('new_password', 'Mot de passe');
-text_input('confirm_new_password', 'Confirmer le mot de passe');
+echo text_input('new_pseudo', 'Pseudo');
+echo text_input('new_password', 'Mot de passe');
+echo text_input('confirm_new_password', 'Confirmer le mot de passe');
 ?>
 <p>Optionel</p>
 <?php
-text_input('new_email', 'Email de récupération', true);
-close_form('Créer le compte');
+echo text_input('new_email', 'Email de récupération', true);
+echo close_form('Créer le compte');
