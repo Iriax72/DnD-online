32acd3e274031345dd782c8ed5ccaa376fbea209
flexult <touma.leandre.72@gmail.com>
2025-10-11 09:50:54 +0000

    patch

--- php/database.php
+++ php/database.php
@@ -6,7 +6,7 @@ $db_user = getenv('DB_USERNAME');
 $db_pass = getenv('DB_PASSWORD');
 
 $pdo = new PDO(
-    "mysql:host=$db_host;port=$db_port;name=$db_name;charset=utf8",
+    "mysql:host=$db_host;port=$db_port;dbname=$db_name;charset=utf8",
     $db_user,
     $db_pass
 );
@@ -15,5 +15,5 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-$sql_file = file_get_contents(__DIR__ . '../sql/accounts.sql');
+$sql_file = file_get_contents(__DIR__ . '/../sql/accounts.sql');
 $pdo->exec($sql_file);
