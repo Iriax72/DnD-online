7010d269bcae899162dc626e35c67f273db8cc0a
flexult <touma.leandre.72@gmail.com>
2025-10-11 12:19:09 +0000

    patch

--- sql/accounts.sql
+++ sql/accounts.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS accounts (
-    'pseudo' (VARCHAR(22)),
-    'password' (VARCHAR(22)),
-    'email' (VARCHAR(50)),
-    'created at' (DATETIME)
+    'pseudo' VARCHAR(22),
+    'password' VARCHAR(22),
+    'email' VARCHAR(50),
+    'created at' DATETIME
 )
