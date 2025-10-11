137905a46a29a7e0872d91ff78d5370ce0e832d2
flexult <touma.leandre.72@gmail.com>
2025-10-11 12:27:57 +0000

    plus court

--- php/database.php
+++ php/database.php
@@ -6,7 +6,7 @@ $db_user = getenv('DB_USERNAME');
 $db_pass = getenv('DB_PASSWORD');
 
 $pdo = new PDO(
-    "mysql:host=$db_host;port=$db_port;charset=utf8",
+    "mysql:host=$db_host;port=$db_port;name=$db_name;charset=utf8",
     $db_user,
     $db_pass
 );
@@ -14,9 +14,9 @@ $pdo = new PDO(
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-echo 'connexion au serveur réussie';
+/*echo 'connexion au serveur réussie';
 $pdo->exec('CREATE DATABASE IF NOT EXISTS accounts');
 echo 'base accounts est crée';
-$pdo->exec('USE accounts');
+$pdo->exec('USE accounts');*/
 $sql_file = file_get_contents(__DIR__ . '/../sql/accounts.sql');
 $pdo->exec($sql_file);
