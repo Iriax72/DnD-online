b1608c842546776b1d0ffb77a58a1ca3439d66eb
flexult <touma.leandre.72@gmail.com>
2025-10-11 08:31:00 +0000

    voila

--- sql/accounts.sql
+++ sql/accounts.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS account (
     'pseudo' (VARCHAR(22)),
     'password' (VARCHAR(22)),
-    'email' (VARCHAR(22)),
+    'email' (VARCHAR(50)),
     'created at' (DATETIME)
 )
