df6dad118f467d2fc31bd94651c27fe620a13999
flexult <touma.leandre.72@gmail.com>
2025-10-12 06:45:18 +0000

    mdr c echo pas alert

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ ini_set('display_errors', 1);
 session_start();
 require __DIR__ . '/php/database.php';
 if (isset($_POST['new_pseudo'])) {
-    alert('ajout du compte dans la db');
+    echo 'ajout du compte dans la db';
 } else if (isset($_POST['pseudo'])) {
-    alert('connexion...');
+    echo 'connexion...';
 }
 
 $_SESSION['connected'] = $_SESSION['connected'] ?? false;
